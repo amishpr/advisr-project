08cd7c9406fb1cdbcba2dd991b17779b539d6bd6
amishpr <amish.prajapati@gmail.com>
2021-05-17 16:56:29 -0400

    Fixed server files

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const serveStatic = require('serve-static')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 8080 
 
-app.use('/', serveStatic(path.join(_dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 app.use(cors())
 
